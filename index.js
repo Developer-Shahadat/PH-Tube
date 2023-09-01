3b039180d35053e1a9d1dd01e63a73597978dee7
Developer-Shahadat <shahadatbci95@gmail.com>
2023-09-02 04:59:22 +0600

    seventh commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const handleCategory = async () => {
     data.data.slice(0,4).forEach((category)=>{
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="tabs ">
+        <div class="tabs">
             <btn onclick = "handleLoadData('${category.category_id}')" class="btn hover:bg-red-400">${category.category}</btn>
         </div>
         `
@@ -28,7 +28,7 @@ const handleLoadData = async (dataLoadId) => {
     if(data.data.length === 0){
         const div = document.createElement('div');
         div.innerHTML =`
-        <img class ="lg:ml-[550px]" src="icons/Icon.png" />
+        <img class ="lg:ml-[550px] ml-[140px] md:ml-[300px]" src="icons/Icon.png" />
         <p class ="mt-2 font-semibold text-3xl">Oops!! Sorry, There is no <br> content here</p>
         `
         container.appendChild(div);
@@ -46,17 +46,17 @@ const handleLoadData = async (dataLoadId) => {
             
         // console.log(hours,minutes);
         div.innerHTML = `
-        <div class="card w-72 bg-base-100 shadow-xl mt-4 pb-8">
+        <div class="card w-72 bg-base-100  mt-4 pb-8">
             <div class=" flex relative">
-            <figure><img class="h-[162px] w-[266px]" src= ${dynamicData?.thumbnail} /></figure>
-            <div class ="absolute mx-28 mt-28 w-36 bg-black text-white">
-            <p class ="text-sm">${hours}hrs ${minutes}mins ago</p>
+            <figure><img class="h-[162px] w-[290px] rounded-xl" src= ${dynamicData?.thumbnail} /></figure>
+            <div class ="absolute mx-32 mt-28 w-36 bg-black rounded-lg text-white">
+            <p class ="text-sm"> ${dynamicData?.others.posted_date? `${hours}hrs ${minutes}mins ago` :  ""} </p>
             </div>
             </div>
             <div class="mt-2 ">
                 
                 <div class="avatar-group ">
-                    <div class="avatar  ">
+                    <div class="avatar ">
                         <div class="w-10">
                         <img src= ${dynamicData.authors[0].profile_picture} />
                     </div>
@@ -64,9 +64,9 @@ const handleLoadData = async (dataLoadId) => {
 
                 <h2 class="text-center mt-3 mx-2 font-medium">${dynamicData?.title}</h2>
             </div>
-            <div class ="flex item-center"> 
-            <p class="mx-14 font-medium text-gray-400">${dynamicData.authors[0].profile_name}</p>
-            <p>${dynamicData?.authors[0].verified? "<img class='' src='icons/check-2.jpg'/>" :  "" }</p>
+            <div class ="flex gap-2 item-center"> 
+            <div> <p class="mx-14 font-medium text-gray-400">${dynamicData.authors[0].profile_name}</p></div>
+            <span>${dynamicData?.authors[0].verified? "<img class='' src='icons/check-2.jpg'/>" :  "" }</span>
             </div>
             <p class ="mx-14 text-sm  text-gray-400 font-medium" >${dynamicData.others.views}  views</p>
             
